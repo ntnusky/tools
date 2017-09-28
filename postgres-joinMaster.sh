fbba7f1560c1dfab5fa880662f6b24aac24a8dd7
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-28 13:48:05 +0200

    Removed accidental linebreak

--- postgres-joinMaster.sh
+++ postgres-joinMaster.sh
@@ -17,8 +17,8 @@ echo Cleaning up old cluster directory
 sudo -u postgres rm -rf /var/lib/postgresql/9.6/main
 
 echo Starting base backup as replicator
-sudo -u postgres pg_basebackup -h $host -D /var/lib/postgresql/9.6/main -U
-replicator -v -P -w -R
+sudo -u postgres pg_basebackup -h $host -D /var/lib/postgresql/9.6/main \
+    -U replicator -v -P -w -R
 
 echo Startging PostgreSQL
 systemctl start postgresql

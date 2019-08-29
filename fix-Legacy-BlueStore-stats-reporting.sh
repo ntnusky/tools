4b6218d69c4792e3d21cc11644ecd570dade6eaf
Eigil Obrestad <eigil-git@obrestad.org>
2019-08-29 14:28:55 +0200

    Add some helpful text

--- fix-Legacy-BlueStore-stats-reporting.sh
+++ fix-Legacy-BlueStore-stats-reporting.sh
@@ -4,7 +4,10 @@ for file in /var/lib/ceph/osd/*; do
   [[ $file =~ [0-9]+$ ]]
   id=${BASH_REMATCH[0]};
 
+  echo "Stopping OSD $id"
   systemctl stop ceph-osd@${id}.service
+  echo "Repairing OSD $id"
   ceph-bluestore-tool repair --path /var/lib/ceph/osd/ceph-${id}
+  echo "Starting OSD $id"
   systemctl start ceph-osd@${id}.service
 done
